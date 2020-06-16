d8422aab5c7562c8c4845290af5ac2fd71024a40
Pierre GRASSER <piorrro33@outlook.com>
2020-06-16 15:45:33 +0200

    Suppressed unused warning for Uint64Adapter

--- src/main/java/com/jcs/overlay/utils/Uint64Adapter.java
+++ src/main/java/com/jcs/overlay/utils/Uint64Adapter.java
@@ -3,6 +3,8 @@ package com.jcs.overlay.utils;
 import com.squareup.moshi.FromJson;
 import com.squareup.moshi.ToJson;
 
+// The methods are used by Moshi for serialization/deserialization.
+@SuppressWarnings("unused")
 public class Uint64Adapter {
     @ToJson
     String toJson(Long aLong) {
