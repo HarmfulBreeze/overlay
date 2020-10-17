fc2d1d5cd876787dff48580ff439fc5a4033a4d0
Pierre GRASSER <piorrro33@outlook.com>
2020-10-17 13:16:33 +0200

    Progress #7 -- Check Last-Modified before sending GET for CDragon assets
    Improves CDragon assets update speed by a lot.

--- src/main/java/com/jcs/overlay/utils/AssetsUpdater.java
+++ src/main/java/com/jcs/overlay/utils/AssetsUpdater.java
@@ -1,5 +1,6 @@
 package com.jcs.overlay.utils;
 
+import com.merakianalytics.orianna.types.core.searchable.SearchableList;
 import com.merakianalytics.orianna.types.core.staticdata.*;
 import com.typesafe.config.ConfigValueFactory;
 import okhttp3.OkHttpClient;
@@ -7,6 +8,10 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 import org.jetbrains.annotations.NotNull;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,10 +23,15 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Locale;
 
 public class AssetsUpdater {
     private static final Logger LOGGER = LoggerFactory.getLogger(AssetsUpdater.class);
     private static final String IMG_FOLDER_PATH = System.getProperty("user.dir") + "/web/img/";
+    private static final DateTimeFormatter RFC1123_FORMATTER = DateTimeFormat
+            .forPattern("EEE, dd MMM yyyy HH:mm:ss 'GMT'")
+            .withZoneUTC()
+            .withLocale(Locale.US);
 
     private AssetsUpdater() {
         throw new UnsupportedOperationException("Utility class");
@@ -56,7 +66,7 @@ public class AssetsUpdater {
             }
             if (latestGamePatch.equals(latestCDragonVersion)) {
                 LOGGER.info("Updated assets are available! Updating.");
-                performCDragonUpdate(client, latestCDragonVersion);
+                performCDragonUpdate(client, latestCDragonVersion, localCDragonPatch);
                 LOGGER.info("CDragon assets update has been successfully completed.");
             } else {
                 LOGGER.warn("Updated assets for patch {} are not available yet. " +
@@ -116,24 +126,42 @@ public class AssetsUpdater {
                 ConfigValueFactory.fromAnyRef(latestVersion));
     }
 
-    private static void performCDragonUpdate(OkHttpClient client, String latestCDragonVersion) {
+    private static void performCDragonUpdate(OkHttpClient client, String latestCDragonPatch, String localCDragonPatch) {
         Champions allChampions = Champions.get();
+        SearchableList<Patch> patchList = Patches.named(localCDragonPatch).get();
+        DateTime localPatchReleaseTime;
+        // if (patchList.size() > 0) {
+        if (patchList.size() > 0 && patchList.get(0).getStartTime() != null) { // hack for not-empty patchList with invalid name
+            localPatchReleaseTime = patchList.get(0).getStartTime().withZone(DateTimeZone.UTC);
+        } else {
+            localPatchReleaseTime = null;
+        }
 
         // Download the generic champion icon and write it to a PNG file
-        String url = "https://raw.communitydragon.org/" + latestCDragonVersion +
+        String url = "https://raw.communitydragon.org/" + latestCDragonPatch +
                 "/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/-1.png";
-        Request request = new Request.Builder().url(url).build();
-        LOGGER.info("Making GET request to " + url);
-        try (Response response = client.newCall(request).execute()) {
-            ResponseBody body = response.body();
-            assert body != null; // Body is non-null as it comes from Call#execute
-            if (response.code() == 200) {
-                Path path = Paths.get(IMG_FOLDER_PATH, "icon/champion/icon_None.png");
-                Files.createDirectories(path.getParent());
-                try (InputStream is = body.byteStream();
-                     OutputStream os = Files.newOutputStream(path)) {
-                    BufferedImage img = ImageIO.read(is);
-                    ImageIO.write(img, "png", os);
+        Request.Builder builder = new Request.Builder().url(url);
+        Request headRequest = builder.head().build();
+        LOGGER.info("Making HEAD request to " + url);
+        try (Response res = client.newCall(headRequest).execute()) {
+            DateTime lastModifiedTime = RFC1123_FORMATTER.parseDateTime(res.header("Last-Modified"));
+            if (localPatchReleaseTime == null || lastModifiedTime.isAfter(localPatchReleaseTime)) {
+                Request getRequest = builder.get().build();
+                LOGGER.info("Making GET request to " + url);
+                try (Response response = client.newCall(getRequest).execute()) {
+                    ResponseBody body = response.body();
+                    assert body != null; // Body is non-null as it comes from Call#execute
+                    if (response.code() == 200) {
+                        Path path = Paths.get(IMG_FOLDER_PATH, "icon/champion/icon_None.png");
+                        Files.createDirectories(path.getParent());
+                        try (InputStream is = body.byteStream();
+                             OutputStream os = Files.newOutputStream(path)) {
+                            BufferedImage img = ImageIO.read(is);
+                            ImageIO.write(img, "png", os);
+                        }
+                    }
+                } catch (IOException e) {
+                    LOGGER.error(e.getMessage(), e);
                 }
             }
         } catch (IOException e) {
@@ -142,21 +170,31 @@ public class AssetsUpdater {
 
         // Download every champion's centered splash art and write them to PNG files
         for (Champion champion : allChampions) {
-            url = "https://raw.communitydragon.org/" + latestCDragonVersion +
+            url = "https://raw.communitydragon.org/" + latestCDragonPatch +
                     "/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/" +
                     champion.getId() + "/" + champion.getId() + "000.jpg";
-            request = new Request.Builder().url(url).build();
-            LOGGER.info("Making GET request to " + url);
-            try (Response response = client.newCall(request).execute()) {
-                ResponseBody body = response.body();
-                assert body != null; // Body is non-null as it comes from Call#execute
-                if (response.code() == 200) {
-                    Path path = Paths.get(IMG_FOLDER_PATH + "splash/" + champion.getKey() + ".png");
-                    Files.createDirectories(path.getParent());
-                    try (InputStream is = body.byteStream();
-                         OutputStream os = Files.newOutputStream(path)) {
-                        BufferedImage img = ImageIO.read(is);
-                        ImageIO.write(img, "png", os);
+            builder = new Request.Builder().url(url);
+            headRequest = builder.head().build();
+            LOGGER.info("Making HEAD request to " + url);
+            try (Response res = client.newCall(headRequest).execute()) {
+                DateTime lastModifiedTime = RFC1123_FORMATTER.parseDateTime(res.header("Last-Modified"));
+                if (localPatchReleaseTime == null || lastModifiedTime.isAfter(localPatchReleaseTime)) {
+                    Request getRequest = builder.get().build();
+                    LOGGER.info("Making GET request to " + url);
+                    try (Response response = client.newCall(getRequest).execute()) {
+                        ResponseBody body = response.body();
+                        assert body != null; // Body is non-null as it comes from Call#execute
+                        if (response.code() == 200) {
+                            Path path = Paths.get(IMG_FOLDER_PATH + "splash/" + champion.getKey() + ".png");
+                            Files.createDirectories(path.getParent());
+                            try (InputStream is = body.byteStream();
+                                 OutputStream os = Files.newOutputStream(path)) {
+                                BufferedImage img = ImageIO.read(is);
+                                ImageIO.write(img, "png", os);
+                            }
+                        }
+                    } catch (IOException e) {
+                        LOGGER.error(e.getMessage(), e);
                     }
                 }
             } catch (IOException e) {
@@ -167,31 +205,41 @@ public class AssetsUpdater {
         // Download every champion tile and write them to PNG files
         for (Champion champion : allChampions) {
             String championKey = champion.getKey();
-            url = "https://raw.communitydragon.org/" + latestCDragonVersion +
+            url = "https://raw.communitydragon.org/" + latestCDragonPatch +
                     "/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/" +
                     champion.getId() + "/" + champion.getId() + "000.jpg";
-            request = new Request.Builder().url(url).build();
-            LOGGER.info("Making GET request to " + url);
-            try (Response response = client.newCall(request).execute()) {
-                ResponseBody body = response.body();
-                assert body != null; // Body is non-null as it comes from Call#execute
-                if (response.code() == 200) {
-                    Path path = Paths.get(IMG_FOLDER_PATH + "tile/" + championKey + ".png");
-                    Files.createDirectories(path.getParent());
-                    try (InputStream is = body.byteStream();
-                         OutputStream os = Files.newOutputStream(path)) {
-                        BufferedImage img = ImageIO.read(is);
-                        ImageIO.write(img, "png", os);
+            builder = new Request.Builder().url(url);
+            headRequest = builder.head().build();
+            LOGGER.info("Making HEAD request to " + url);
+            try (Response res = client.newCall(headRequest).execute()) {
+                DateTime lastModifiedTime = RFC1123_FORMATTER.parseDateTime(res.header("Last-Modified"));
+                if (localPatchReleaseTime == null || lastModifiedTime.isAfter(localPatchReleaseTime)) {
+                    Request getRequest = builder.get().build();
+                    LOGGER.info("Making GET request to " + url);
+                    try (Response response = client.newCall(getRequest).execute()) {
+                        ResponseBody body = response.body();
+                        assert body != null; // Body is non-null as it comes from Call#execute
+                        if (response.code() == 200) {
+                            Path path = Paths.get(IMG_FOLDER_PATH + "tile/" + championKey + ".png");
+                            Files.createDirectories(path.getParent());
+                            try (InputStream is = body.byteStream();
+                                 OutputStream os = Files.newOutputStream(path)) {
+                                BufferedImage img = ImageIO.read(is);
+                                ImageIO.write(img, "png", os);
+                            }
+                        }
+                    } catch (IOException e) {
+                        LOGGER.error(e.getMessage(), e);
                     }
                 }
-            } catch (IOException e) {
+            } catch (Exception e) {
                 LOGGER.error(e.getMessage(), e);
             }
         }
 
         // Update latest patch in config
         SettingsManager.getManager().updateValue("debug.cdragonPatch",
-                ConfigValueFactory.fromAnyRef(latestCDragonVersion));
+                ConfigValueFactory.fromAnyRef(latestCDragonPatch));
     }
 
     /**
